8d69eb66f4d2517eb6369a1cb45f971b54a5e400
kristen.peck <kristen.peck@dfo-mpo.gc.ca>
2022-02-08 20:00:52 -0800

    added csv

--- copyvideofiles.R
+++ copyvideofiles.R
@@ -52,10 +52,10 @@ cam.data2 <- read_csv("2021VideoData_4Nov-25Nov2021.csv",
 str(cam.data2)
 
 
-#select the data you want to extract. 
+# extract selected videos #### 
 # Note that you have to do this one harddrive at a time
 
-select_rows <- cam.data1 %>% 
+select_rows <- cam.data2 %>% 
   filter(BTDV >= 1) %>% 
   select(files)
 
@@ -70,28 +70,22 @@ file.copy(from=select_files,
           to="E:/copiedvideo-BTDV")
 
 
+# Extract QAd videos ####
 
 #randomly select videos for QA - need to do this one harddrive at a time
+#note that it will re-randomize every time you run this
 
-sample <- sample(nrow(cam.data2),round(nrow(cam.data2)*0.01,0), 
+sample <- sample(nrow(cam.data1),round(nrow(cam.data2)*0.01,0), 
                  replace = F)
 
-select_rows <- cam.data2[sample,]
+select_rows <- cam.data1[sample,]
 
-(select_files <- paste0("E:/",select_rows$files))
+(select_files <- paste0("F:/",select_rows$files))
 
 file.copy(from=select_files,
-          to="C:/Users/Peckk/OneDrive - DFO-MPO/Documents/R/fishcam/QA/QA2",
+          to="C:/Users/Peckk/OneDrive - DFO-MPO/Documents/R/fishcam/QA/QA1",
           copy.date = T)
 
-write_csv(select_rows, "QA/QA1.csv",na = "")
+write_csv(select_rows, "QA/QA1/QA1.csv",na = "")
 
-#
-# cam.data2$filenames <- substr(cam.data2$files,20,60)
-# setwd("C:/Users/Peckk/OneDrive - DFO-MPO/Documents/R/fishcam/QA/QA2")
-# files <- list.files(path = ".",pattern="*.m4v",recursive = T) 
-# files <- data.frame(filenames=files) %>% 
-#   left_join(cam.data2)
-# 
-# write_csv(files, "QA2.csv",na = "")
-# getwd()
+
